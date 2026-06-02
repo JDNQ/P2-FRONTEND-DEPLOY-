eb0a4789f0d86494dcd8546a50ff970e7187f8da
JDNQ <108049503+JDNQ@users.noreply.github.com>
2026-06-02 11:12:13 +0700

    1

--- product-fe/app/layout.tsx
+++ product-fe/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col bg-white text-gray-900">
         {children}
+        <SpeedInsights />
         <Analytics />
       </body>
     </html>
