aa08df6e5b60fefd25a6ae9625323be4c45958a3
JDNQ <108049503+JDNQ@users.noreply.github.com>
2026-06-08 17:22:56 +0700

    1

--- product-fe/components/LayoutClient.tsx
+++ product-fe/components/LayoutClient.tsx
@@ -1,13 +1,21 @@
 "use client"
+import { usePathname } from 'next/navigation'
 import { AppProvider } from "@/lib/store"
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
 import Toast from "@/components/Toast"
 
 export default function LayoutClient({ children }: { children: React.ReactNode }) {
+    const pathname = usePathname()
+
+    const hideHeader =
+        pathname === "/login" ||
+        pathname === "/register" ||
+        pathname.startsWith("/dashboard")
+
     return (
         <AppProvider>
-            <Header />
+            {!hideHeader && <Header />}
             <main className="min-h-screen">{children}</main>
             <Footer />
             <Toast />
